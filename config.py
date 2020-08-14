b49701b9b149384807e070efa5f4e963d7846f8d
Marc Basquens <marc.basquens@basetis.com>
2020-08-14 08:49:51 +0200

    Stack features

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ def read_data(symbol):
     file_name = get_file_name(symbol)
     df = pd.read_csv(file_name, header=0)
     return df
+
+
+def get_downloaded_symbols():
+    files_list = os.listdir(data_path)
+    return [f.replace(".csv", "") for f in files_list if os.path.isfile(os.path.join(data_path, f))]
--- data_processing.py
+++ data_processing.py
@@ -1,17 +1,28 @@
 from config import *
+import random
+import tensorflow as tf
 import pandas as pd
 from datetime import datetime
 from visualization import plot
 
 
+# We need to make sure the output is of the same shape
 def select_features(symbol, from_date=datetime(2018, 1, 1), to_date=datetime(2019, 1, 1)):
     df = read_data(symbol)
     start_date = datetime.timestamp(from_date)
     end_date = datetime.timestamp(to_date)
-    selected_range = df.loc[(df.t >= start_date) & (df.t <= end_date)][['c', 't']]
-    return selected_range
+    selected_range = df.loc[(df.t >= start_date) & (df.t <= end_date)][['c']]
+    return tf.convert_to_tensor(selected_range)
+
+
+def main():
+    symbols = get_downloaded_symbols()
+    candidates = random.sample(symbols, 100)
+    result = tf.stack([select_features(candidate) for candidate in candidates])
+    print(result)
 
 
 if __name__ == "__main__":
-    df = select_features('A')
-    plot(df)
+    #df = select_features('A')
+    #plot(df)
+    main()
